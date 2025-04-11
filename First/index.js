4757b698fb36cf85ce958a170cc45bd78351a6b3
user.name "Satyam Shinde <user.email "satyamshinde301@gmail.com>
2025-04-11 11:05:02 +0530

    Ep_3_Part_3

--- First/index.js
+++ First/index.js
@@ -1,24 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Parent = React.createElement('h1', { id: "header", className: "header" },
-     [React.createElement('div', 
-        { id: "Child1", className: "Child1" },
-        [React.createElement('h1', 
-            { id: "h1", className: "h1" },
-             "hello world with SSSSSSssssSReact Js!"),React.createElement('h2', 
-                { id: "h2", className: "h2" },
-                 "hello world with React Js!")]), 
-                 React.createElement('div', 
-                    { id: "Child2", className: "Child2" },
-                    [React.createElement('h1', 
-                        { id: "h1", className: "h1" },
-                         "hello world with React Js!"),React.createElement('h2', 
-                            { id: "h2", className: "h2" },
-                             "hello world with React Js!")])
-            ],)
+// const Parent = React.createElement('h1', { id: "header", className: "header" },
+//      [React.createElement('div', 
+//         { id: "Child1", className: "Child1" },
+//         [React.createElement('h1', 
+//             { id: "h1", className: "h1" },
+//              "hello world with SSSSSSssssSReact Js!"),React.createElement('h2', 
+//                 { id: "h2", className: "h2" },
+//                  "hello world with React Js!")]), 
+//                  React.createElement('div', 
+//                     { id: "Child2", className: "Child2" },
+//                     [React.createElement('h1', 
+//                         { id: "h1", className: "h1" },
+//                          "hello world with React Js!"),React.createElement('h2', 
+//                             { id: "h2", className: "h2" },
+//                              "hello world with React Js!")])
+//             ],)
 
+const JsxParent = (<>
+   <h1>Hi iam from jsx code</h1>
+   <h1>Hi iam from jsx code</h1>
+   <h1>Hi iam from jsx code</h1>
+   <h1>Hi iam from jsx code</h1>
+</>
+)
+console.log(JsxParent);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(Parent)
+root.render(JsxParent)
